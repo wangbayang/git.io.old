b31ac994ec55ed3b254596519b3bdf9800c8d25f
keby <771975543@qq.com>
2018-11-20 14:00:27 +0800

    test

---
+++ precache-manifest.894e2138b3313ddd4305b03f4b48b799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f64eb6e777e120e52c0f",
+    "url": "/static/js/main.f64eb6e7.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "f64eb6e777e120e52c0f",
+    "url": "/static/css/main.3716520f.chunk.css"
+  },
+  {
+    "revision": "8165ffc5c4cd1fd12516810a3f74c35b",
+    "url": "/index.html"
+  }
+];
