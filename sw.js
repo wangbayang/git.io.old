efd368e38a9a47017aab6b927f5a5a31a6eb169e
keby <771975543@qq.com>
2018-11-23 11:57:04 +0800

    auto

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
+const cacheVersion = 'v0.0.1';
+
 this.addEventListener('install', event => {
   console.log('install');
   // 新线程安装后跳过等待阶段，直接应用新的线程
   this.skipWaiting();
-  // waitUntil() 方法会确保 Service Worker 不会在 waitUntil() 里面的代码执行完毕之前安装完成。
-  event.waitUntil(
-    caches.open('vue-v1').then(cache => cache.add('/vue.png'))
-  );
+  // 传入一个 Promise 为参数，等到该 Promise 为 resolve 状态为止。
+  // event.waitUntil(
+  //   caches.open('vue-v1').then(cache => cache.add('/vue.png'))
+  // );
   // event.waitUntil(
   //   caches.keys().then(keys => {
   //     console.log(keys)
@@ -15,15 +17,29 @@ this.addEventListener('install', event => {
 
 this.addEventListener('activate', event => {
   console.log('activate');
-  // 在新线程激活后立即控制之前没有控制的客户端，从而达到第一次创建线程就可以劫持fetch，不用再刷新一次。
-  clients.claim();
+  event.waitUntil(
+    Promise.all([
+      // 在新线程激活后立即控制之前没有控制的客户端，从而达到第一次创建线程就可以劫持fetch，不用再刷新一次。
+      clients.claim(),
+      // 清理旧版本
+      caches.keys().then(cacheList => {
+        return Promise.all(
+          cacheList.map(cacheName => {
+            if (cacheName !== cacheVersion) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    ])
+  );
 })
 
 this.addEventListener('fetch', event => {
-  console.log('fetch', event.request)
+  console.log('request', event.request.url);
   event.respondWith(
-    caches.match(event.request).then(res => {
-      console.log(res)
+    caches.match(event.request.url).then(res => {
+      console.log('response', res && res.url);
       if (res) {
         return res;
       }
@@ -36,18 +52,30 @@ this.addEventListener('fetch', event => {
         }
 
         const httpResClone = httpRes.clone();
-        caches.open('my-cache-v1').then(cache => {
-          cache.put(event.request, httpResClone);
+        caches.open(cacheVersion).then(cache => {
+          cache.put(event.request.url, httpResClone);
         });
 
         return httpRes;
       });
     })
-    .catch(e => console.log(e))
+      .catch(e => console.log(e))
   );
-  // const url = new URL(event.request.url);
-  // if (url.origin === location.origin && url.pathname === '/logo.svg') {
-  //   // if (!caches.match('/vue.png')) return;
-  //   event.respondWith(caches.match('/vue.png'));
-  // }
 })
+
+this.addEventListener('message', event => {
+  if (event.data === 'start test performance') {
+    for(let i = 0; i < 5000; i++) {
+      console.log('test')
+    }
+    clients.matchAll()
+      .then(function (clients) {
+        if (clients && clients.length) {
+          clients.forEach(function (client) {
+            // 发送字符串'sw.update'
+            client.postMessage('down');
+          })
+        }
+      })
+  }
+});
